2c64eb744fba73160a80574707f74d359c6453a3
srikanthpragada <srikanthpragada@gmail.com>
2021-08-27 17:54:57 +0530

    27-AUG

---
+++ equal_test.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+void main()
+{
+  int a = 10, b = 20;
+
+    if (a = b)
+       printf("Equal");
+    else
+       printf("Not Equal");
+
+}
---
+++ no_days_with_switch.c
@@ -0,0 +1,25 @@
+// Program to display no. of days in the given month
+
+#include <stdio.h>
+
+void main()
+{
+   int month;
+
+     // Input
+     printf("Enter month :");
+     scanf("%d", &month);
+
+     switch(month)
+     {
+         case 2:  printf("28");
+                  break;
+         case 4:
+         case 6:
+         case 9:
+         case 11: printf("30");
+                  break;
+         default:
+                  printf("31");
+     } // switch
+} // main
