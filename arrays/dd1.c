63f194487c39bbebd19787eb8635e1dafa3bf8ab
srikanthpragada <srikanthpragada@gmail.com>
2021-09-07 17:56:08 +0530

    07-SEP

---
+++ arrays/dd1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+  int a[5][5];
+  int i,j;
+
+       srand(time(0));
+
+       for(i = 0; i < 5;i ++)
+       {
+         for(j = 0; j < 5; j ++)
+         {
+            a[i][j] = rand() % 100;
+            printf("%5d", a[i][j]);
+         }
+         printf("\n");
+       }
+
+}
